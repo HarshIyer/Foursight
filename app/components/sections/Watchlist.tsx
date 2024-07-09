09914640ef969f19db521b108d2362a53691bf1f
HarshIyer <git@hi05.xyz>
2024-07-10 00:28:53 +0530

    Fix linting errors

--- app/components/sections/Watchlist.tsx
+++ app/components/sections/Watchlist.tsx
@@ -15,8 +15,8 @@ export default function Watchlist() {
           change={5}
           changePercent={0.5}
           opening={2400}
+          equityType="STOCKS"
           closing={2450}
-          avg7dVolume="7.15M"
           yearlyHigh={2600}
           yearlyLow={2300}
           marketCap="21.65T"
@@ -28,8 +28,8 @@ export default function Watchlist() {
           change={5}
           changePercent={0.5}
           opening={2400}
+          equityType="STOCKS"
           closing={2450}
-          avg7dVolume="7.15M"
           yearlyHigh={2600}
           yearlyLow={2300}
           marketCap="21.65T"
@@ -41,8 +41,8 @@ export default function Watchlist() {
           change={5}
           changePercent={0.5}
           opening={2400}
+          equityType="STOCKS"
           closing={2450}
-          avg7dVolume="7.15M"
           yearlyHigh={2600}
           yearlyLow={2300}
           marketCap="21.65T"
@@ -54,21 +54,8 @@ export default function Watchlist() {
           change={5}
           changePercent={0.5}
           opening={2400}
+          equityType="STOCKS"
           closing={2450}
-          avg7dVolume="7.15M"
-          yearlyHigh={2600}
-          yearlyLow={2300}
-          marketCap="21.65T"
-        />
-        <Scrip
-          title="Reliance Industries"
-          ltp={2500}
-          symbol="RELIANCE"
-          change={5}
-          changePercent={0.5}
-          opening={2400}
-          closing={2450}
-          avg7dVolume="7.15M"
           yearlyHigh={2600}
           yearlyLow={2300}
           marketCap="21.65T"
