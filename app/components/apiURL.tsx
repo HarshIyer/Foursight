1f957d451dccef3a2b4921642c7239716f4bf6ab
HarshIyer <git@hi05.xyz>
2024-07-25 21:20:01 +0530

    Change api url

--- app/components/apiURL.tsx
+++ app/components/apiURL.tsx
@@ -1,2 +1,2 @@
-// export const apiURL = "https://foursight-backend.harshiyer.workers.dev/api/v1";
-export const apiURL = "http://localhost:8787/api/v1";
+export const apiURL = "https://foursight-backend.harshiyer.workers.dev/api/v1";
+// export const apiURL = "http://localhost:8787/api/v1";
